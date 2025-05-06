fafe47b44ecaec505c82bfb95f9dfe8ccee169cb
codewithgun <codewithgun@gmail.com>
2025-05-06 16:08:10 +0800

    chore: update anchor version

--- programs/alpha-vault/src/seeds.rs
+++
@@ -1,6 +0,0 @@
-pub const CRANK_FEE_WHITELIST: &[u8] = b"crank_fee_whitelist";
-pub const MERKLE_ROOT: &[u8] = b"merkle_root";
-pub const ESCROW: &[u8] = b"escrow";
-pub const FCFS_CONFIG: &[u8] = b"fcfs_config";
-pub const VAULT: &[u8] = b"vault";
-pub const PRORATA_CONFIG: &[u8] = b"prorata_config";
--- programs/alpha-vault/src/state/crank_fee_whitelist.rs
+++
@@ -1,8 +0,0 @@
-use crate::*;
-
-#[account(zero_copy)]
-#[derive(InitSpace)]
-pub struct CrankFeeWhitelist {
-    pub owner: Pubkey,
-    pub padding: [u128; 5],
-}
