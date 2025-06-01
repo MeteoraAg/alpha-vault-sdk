cc9ed0aa7bd58d037d86d59472839fb7af83b2e3
Dong Nguyen <dongnguyen9186@gmail.com>
2025-06-01 11:15:58 +0700

    chore: export IDL

--- ts-client/src/index.ts
+++ ts-client/src/index.ts
@@ -1,4 +1,5 @@
 import { AlphaVault } from "./alpha-vault";
+import IDL from "../../idls/alpha_vault.json";
 
 export default AlphaVault;
 export * from "./alpha-vault/type";
@@ -6,3 +7,5 @@ export * from "./alpha-vault/constant";
 export * from "./alpha-vault/idl";
 export * from "./alpha-vault/merkle_tree";
 export * from "./alpha-vault/helper";
+
+export { IDL };
